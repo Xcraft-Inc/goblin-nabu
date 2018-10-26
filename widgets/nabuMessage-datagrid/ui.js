89628988b982e43ef575db0672cd1148c68379c9
Jonny Quarta <quarta@epsitec.ch>
2018-10-26 12:08:43 +0200

    Add simple nabuMessage datagrid row ui definition

--- widgets/nabuMessage-datagrid/ui.js
+++ widgets/nabuMessage-datagrid/ui.js
@@ -2,7 +2,17 @@ import React from 'react';
 
 import Container from 'gadgets/container/widget';
 import Field from 'gadgets/field/widget';
-import Label from 'gadgets/label/widget';
+
+function renderRow(props) {
+  return (
+    <Container kind="row">
+      <Field kind="label" grow="1" labelWidth="0px" model=".nabuId" />
+      <Field model=".description" grow="1" labelWidth="0px" />
+    </Container>
+  );
+}
 
 /******************************************************************************/
-export default {};
+export default {
+  row: renderRow,
+};
