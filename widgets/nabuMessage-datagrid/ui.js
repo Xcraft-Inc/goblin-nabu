bda997ff7de0506d984d8a81f42c574f0dff53ab
Jonny Quarta <quarta@epsitec.ch>
2018-10-30 14:56:30 +0100

    Add rendering of headers in nabu datagrid

--- widgets/nabuMessage-datagrid/ui.js
+++ widgets/nabuMessage-datagrid/ui.js
@@ -1,8 +1,58 @@
 import React from 'react';
+import Widget from 'laboratory/widget';
 
 import Field from 'gadgets/field/widget';
 import Connect from 'laboratory/connect';
 import Label from 'gadgets/label/widget';
+import TextFieldCombo from 'gadgets/text-field-combo/widget';
+
+// ------------------------------------------------------------
+class HeaderCombo extends Widget {
+  render() {
+    const {locales, index, datagrid} = this.props;
+    const localesList = locales.map(l => l.get('name')).toJS();
+
+    return (
+      <TextFieldCombo
+        model={`.columns[${index}].field`}
+        readonly="true"
+        grow="1"
+        list={localesList}
+        menuType="wrap"
+        defaultValue={''}
+        comboTextTransform="none"
+        onSetText={locale => {
+          datagrid.do('changeSelectedLocale', {index, locale});
+        }}
+      />
+    );
+  }
+}
+
+function renderMissingTranslationsHeaderCell() {
+  return <div />;
+}
+
+function renderNabuIdHeaderCell() {
+  return (
+    <Label
+      spacing="overlap"
+      text={{
+        id: 'Message original',
+      }}
+    />
+  );
+}
+
+function renderLocaleHeaderCell(index, datagrid) {
+  return (
+    <Connect locales={state => state.get(`backend.nabu.locales`)}>
+      <HeaderCombo index={index} datagrid={datagrid} />
+    </Connect>
+  );
+}
+
+// ------------------------------------------------------------
 
 function renderMissingTranslationsRowCell(id) {
   return (
@@ -64,6 +114,22 @@ function renderLocaleRowCell(id, field) {
   return <Field model={`.${field}`} grow="1" labelWidth="0px" />;
 }
 
+// ------------------------------------------------------------
+
+function renderHeaderCell(props) {
+  switch (props.column.get('name')) {
+    case 'missingTranslations':
+      return renderMissingTranslationsHeaderCell();
+    case 'nabuId':
+      return renderNabuIdHeaderCell();
+    case 'locale_1':
+    case 'locale_2':
+      return renderLocaleHeaderCell(props.index, props.datagrid);
+    default:
+      return <div />;
+  }
+}
+
 function renderRowCell(props) {
   switch (props.column.get('name')) {
     case 'missingTranslations':
@@ -80,5 +146,6 @@ function renderRowCell(props) {
 
 /******************************************************************************/
 export default {
+  headerCell: renderHeaderCell,
   rowCell: renderRowCell,
 };
