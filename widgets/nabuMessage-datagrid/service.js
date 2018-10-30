a56f92ff1d1154a743ca69c7af53416ef2f46536
Jonny Quarta <quarta@epsitec.ch>
2018-10-30 13:26:30 +0100

    Fill columns prop in nabuMessage datagrid

--- widgets/nabuMessage-datagrid/service.js
+++ widgets/nabuMessage-datagrid/service.js
@@ -6,7 +6,34 @@ const config = {
   type: 'nabuMessage',
   kind: 'datagrid',
   initialStatuses: ['draft', 'published'],
+  initialState: {},
   pageSize: 15,
+  columns: [
+    {
+      name: 'missingTranslations',
+    },
+    {
+      name: 'nabuId',
+      field: 'nabuId',
+      sortable: true,
+      filterable: true,
+    },
+    {
+      name: 'locale_1',
+      field: null,
+      sortable: true,
+      filterable: true,
+    },
+    {
+      name: 'locale_2',
+      field: null,
+      sortable: true,
+      filterable: true,
+    },
+  ],
+  afterCreate: function(quest) {
+    console.log('after create');
+  },
 };
 
 module.exports = buildWorkitem(config);
