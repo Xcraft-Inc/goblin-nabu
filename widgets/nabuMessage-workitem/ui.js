31ef3b3fe5ebca73bcd0f4170ddb5199524005b9
Catia Guidi <guidi@epsitec.ch>
2018-11-23 16:04:43 +0100

    Add dialog kind for workitem

--- widgets/nabuMessage-workitem/ui.js
+++ widgets/nabuMessage-workitem/ui.js
@@ -1,32 +1,42 @@
 import React from 'react';
 import Widget from 'laboratory/widget';
+import Form from 'laboratory/form';
 
 import Container from 'gadgets/container/widget';
 import Field from 'gadgets/field/widget';
 import TranslationFieldConnected from '../nabuWidgets/translationField';
 
 /******************************************************************************/
-class NabuMessage extends Widget {
+class NabuMessage extends Form {
   constructor() {
     super(...arguments);
   }
 
+  static get wiring() {
+    return {
+      id: 'id',
+    };
+  }
+
   render() {
     const {locales} = this.props;
+    const Form = this.Form;
 
     return (
       <Container kind="column">
         <Container kind="pane">
-          <Container kind="row-pane">
-            <Field kind="label" grow="1" labelWidth="0px" model={`.nabuId`} />
-          </Container>
-          <Container kind="row-pane">
-            <Field
-              labelText="Description"
-              model=".description"
-              readonly={true}
-            />
-          </Container>
+          <Form {...this.formConfig}>
+            <Container kind="row-pane">
+              <Field kind="label" grow="1" labelWidth="0px" model={`.nabuId`} />
+            </Container>
+            <Container kind="row-pane">
+              <Field
+                labelText="Description"
+                model=".description"
+                readonly={true}
+              />
+            </Container>
+          </Form>
 
           {locales.map(l => {
             const translationId = `nabuTranslation@${l.get('name')}-${
@@ -52,7 +62,7 @@ class NabuMessage extends Widget {
   }
 }
 
-const NabuMessageConnected = Widget.connect((state, props) => ({
+const NabuMessageConnected = Widget.connect(state => ({
   locales: state.get(`backend.nabu.locales`),
 }))(NabuMessage);
 
