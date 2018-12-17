fdae7245e56c8d48511916f06256b41a49b6eb1a
Catia Guidi <guidi@epsitec.ch>
2018-12-17 16:41:16 +0100

    WIP: call elasticSearch for filtering and/or sorting datagrid data

--- widgets/nabuMessage-datagrid/service.js
+++ widgets/nabuMessage-datagrid/service.js
@@ -36,6 +36,14 @@ const config = {
       customSort: true,
     },
   ],
+  hinter: {
+    type: 'nabuMessage',
+    subTypes: ['nabuTranslation'],
+    subJoins: ['ownerId'],
+    field: 'id',
+    fields: ['info'],
+    title: 'Messages',
+  },
   hasTranslations: {},
   afterCreate: function*(quest, next) {
     // Setting correct selected locales
@@ -70,19 +78,10 @@ const config = {
     });
 
     // Loading translations
-    const hinter = {
-      type: 'nabuMessage',
-      subTypes: ['nabuTranslation'],
-      subJoins: ['ownerId'],
-      field: 'id',
-      fields: ['info'],
-      title: 'Messages',
-    };
+    const datagridId = quest.goblin.getX('datagridId');
+    const datagridAPI = quest.getAPI(datagridId);
+    const listIds = yield datagridAPI.getListIds();
 
-    const listIds = yield quest.me.returnSearch({
-      hinter,
-      sort: {key: 'value.keyword', dir: 'asc'},
-    });
     yield quest.me.loadTranslations({listIds}, next);
     yield quest.me.setNeedTranslation();
   },
@@ -104,95 +103,13 @@ const config = {
         next
       );
 
-      const filters = quest.goblin.getState().get(`filters`);
       const sort = quest.goblin.getState().get('sort');
 
-      if (
-        (filters.get(currentLocale) != undefined &&
-          filters.get(currentLocale) !== '') ||
-        sort.get('key') === currentLocale
-      ) {
+      if (sort.get('key') === currentLocale) {
         yield quest.me.resetListVisualization(next);
       }
 
       yield quest.me.setNeedTranslation(next);
-    },
-    returnSearch: function*(quest, hinter, value, sort) {
-      const elastic = quest.getStorage('elastic');
-
-      let type = hinter.type;
-      const subTypes = hinter.subTypes;
-      if (subTypes) {
-        subTypes.forEach(subType => {
-          type = `${type},${subType}`;
-        });
-      }
-
-      const results = yield elastic.search({
-        type,
-        value,
-        sort,
-        from: 0,
-        size: 200,
-      });
-
-      let values = [];
-      if (results) {
-        results.hits.hits.map(hit => {
-          if (!hit.highlight) {
-            return hit._source.info;
-          }
-
-          let phonetic = false;
-          let autocomplete = false;
-
-          if (hit.highlight.searchPhonetic) {
-            phonetic = true;
-          }
-          if (hit.highlight.searchAutocomplete) {
-            autocomplete = true;
-          }
-
-          if (!phonetic && !autocomplete) {
-            return hit._source.info;
-          }
-
-          // Prefer phonetic result if possible, but use autocomplete result
-          // if there are more tags.
-          if (phonetic && autocomplete) {
-            const countPhonetic = (
-              hit.highlight.searchPhonetic[0].match(/<em>/g) || []
-            ).length;
-            const countAutocomplete = (
-              hit.highlight.searchAutocomplete[0].match(/<em>/g) || []
-            ).length;
-            if (countAutocomplete > countPhonetic) {
-              phonetic = false;
-            }
-          }
-
-          return phonetic
-            ? hit.highlight.searchPhonetic[0].replace(/<\/?em>/g, '`')
-            : hit.highlight.searchAutocomplete[0].replace(/<\/?em>/g, '`');
-        });
-
-        results.hits.hits.forEach(hit => {
-          let value = hit._id;
-          if (hinter.subJoins) {
-            hinter.subJoins.forEach(subJoin => {
-              const join = hit._source[subJoin];
-              if (join) {
-                value = join;
-              }
-            });
-          }
-          if (!values.includes(value)) {
-            values.push(value);
-          }
-        });
-      }
-
-      return values;
     },
     applyElasticVisualization: function*(quest, value, sort, next) {
       if (value !== undefined) {
@@ -209,41 +126,23 @@ const config = {
         yield quest.me.toggleSort({field: sort}, next);
       }
 
-      const idsGetter = watt(function*() {
-        const hinter = {
-          type: 'nabuMessage',
-          subTypes: ['nabuTranslation'],
-          subJoins: ['ownerId'],
-          field: 'id',
-          fields: ['info'],
-          title: 'Messages',
-        };
-        const sortValue = quest.goblin.getState().get('sort');
-        let key = 'value.keyword';
-        const sortKey = sortValue.get('key');
-        if (sortKey !== 'nabuId') {
-          key = `${sortKey}-value.keyword`;
-        }
-
-        const searchValue = quest.goblin.getState().get('searchValue');
-
-        return yield quest.me.returnSearch({
-          hinter,
-          value: searchValue,
-          sort: {
-            key,
-            dir: sortValue.get('dir'),
-          },
-        });
-      });
+      const sortValue = quest.goblin.getState().get('sort');
+      let key = 'value.keyword';
+      const sortKey = sortValue.get('key');
+      if (sortKey !== 'nabuId') {
+        key = `${sortKey}-value.keyword`;
+      }
+      const searchValue = quest.goblin.getState().get('searchValue');
 
       const datagridId = quest.goblin.getX('datagridId');
       const datagridAPI = quest.getAPI(datagridId);
-      yield datagridAPI.customizeVisualization(
-        {listIdsGetter: idsGetter},
-        next
-      );
-      //quest.defer (() => quest.me.loadMessages ());
+      yield datagridAPI.customizeVisualization({
+        filter: searchValue,
+        sort: {
+          key,
+          dir: sortValue.get('dir'),
+        },
+      });
     },
     loadTranslations: function(quest, listIds) {
       const nabuApi = quest.getAPI('nabu');
