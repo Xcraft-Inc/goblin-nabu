cebfcb45297e70f495f930afaec79d8d68790389
Jonny Quarta <quarta@epsitec.ch>
2019-02-26 14:09:41 +0100

    Avoid undefined values in nabu objects

--- widgets/helpers/t.js
+++ widgets/helpers/t.js
@@ -1,8 +1,19 @@
 module.exports = function ToNabuObject(nabuId, description, values, html) {
-  return {
-    nabuId,
-    description,
-    html,
-    values,
-  };
+  const nabuObject = {};
+
+  // This is done so that we avoid having undefined fields (rethinkdb does not like it)
+  if (nabuId) {
+    nabuObject.nabuId = nabuId;
+  }
+  if (description) {
+    nabuObject.description = description;
+  }
+  if (values) {
+    nabuObject.values = values;
+  }
+  if (html) {
+    nabuObject.html = html;
+  }
+
+  return nabuObject;
 };
