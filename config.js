f053b48c286a05bd2455bc8e4544fd5835c034bf
Yannick Vessaz <vessaz@epsitec.ch>
2019-04-02 14:28:15 +0200

    Load nabu services using elastic only if storageAvailable config is set

---
+++ config.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * Retrieve the inquirer definition for xcraft-core-etc
+ */
+module.exports = [
+  {
+    type: 'confirm',
+    name: 'storageAvailable',
+    message: 'Storage available',
+    default: true,
+  },
+];
