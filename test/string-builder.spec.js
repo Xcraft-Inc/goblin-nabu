24eb30e345a51a469eb671b356c3bb3e1631adc0
Daniel Roux <roux@epsitec.ch>
2019-05-23 15:55:12 +0200

    WIP: Invert parameters.

--- test/string-builder.spec.js
+++ test/string-builder.spec.js
@@ -7,84 +7,84 @@ const T = require('goblin-nabu/widgets/helpers/t.js');
 describe('StringBuilder Combine', function() {
   it('#Test Strings', function() {
     const result = StringBuilder.combine('a', 'b', 'c');
-    assert.equal('abc', result);
+    assert.strictEqual(result, 'abc');
   });
 
   it('#Test Numbers', function() {
     const result = StringBuilder.combine(1, 2, 3);
-    assert.equal('123', result);
+    assert.strictEqual(result, '123');
   });
 
   it('#Test NullsAndStringw', function() {
     const result = StringBuilder.combine('a', null, 'c');
-    assert.equal('ac', result);
+    assert.strictEqual(result, 'ac');
   });
 
   it('#Test NullsAndStrings2', function() {
     const result = StringBuilder.combine(null, 'b', 'c');
-    assert.equal('bc', result);
+    assert.strictEqual(result, 'bc');
   });
 
   it('#Test StringsAndT', function() {
     const result = StringBuilder.combine('hello ', T('world'));
-    assert.equal('translatableString', result._type);
+    assert.strictEqual(result._type, 'translatableString');
     assert.ok(result._string);
-    assert.equal(2, result._string.length);
+    assert.strictEqual(result._string.length, 2);
   });
 
   it('#Test NullAndT', function() {
     const result = StringBuilder.combine(null, T('world'));
-    assert.equal('translatableString', result._type);
+    assert.strictEqual(result._type, 'translatableString');
     assert.ok(result._string);
-    assert.equal(1, result._string.length);
+    assert.strictEqual(result._string.length, 1);
   });
 
   it('#Test MultipleTAndStrings', function() {
     const result = StringBuilder.combine(T('hello'), T('world'), '!!!');
-    assert.equal('translatableString', result._type);
+    assert.strictEqual(result._type, 'translatableString');
     assert.ok(result._string);
-    assert.equal(3, result._string.length);
+    assert.strictEqual(result._string.length, 3);
   });
 
   it('#Test MultipleTranslatableStrings', function() {
     const result1 = StringBuilder.combine(T('hello'), ',', ' ');
     const result2 = StringBuilder.combine(T('world'), '!!!');
     const result3 = StringBuilder.combine(result1, result2);
-    assert.equal('translatableString', result3._type);
+    assert.strictEqual(result3._type, 'translatableString');
     assert.ok(result3._string);
-    assert.equal(4, result3._string.length);
+    assert.strictEqual(result3._string.length, 4);
   });
 });
 
 describe('StringBuilder Join', function() {
   it('#Test Strings', function() {
     const result = StringBuilder.join(['a', 'b', 'c'], ',');
-    assert.equal('a,b,c', result);
+    assert.strictEqual(result, 'a,b,c');
   });
 
   it('#Test Nulls', function() {
     const result = StringBuilder.join([null, null], ',');
-    assert.equal(null, result);
+    assert.strictEqual(result, null);
   });
 
   it('#Test StringsAndNulls', function() {
     const result = StringBuilder.joinSentences([null, 'a', null, 'b']);
-    assert.equal('a, b', result);
+    assert.strictEqual(result, 'a, b');
   });
 
   it('#Test StringsAndT', function() {
     const result = StringBuilder.joinSentences([null, 'a', T('b'), 'c']);
-    assert.equal('translatableString', result._type);
+    assert.strictEqual(result._type, 'translatableString');
     assert.ok(result._string);
-    assert.equal(3, result._string.length);
+    assert.strictEqual(result._string.length, 3);
   });
 
   it('#Test TAndTranslatableStrings', function() {
     const result = StringBuilder.joinSentences([null, 'a', T('b'), 'c']);
     const result2 = StringBuilder.joinSentences([result, null, T('d')]);
-    assert.equal('translatableString', result2._type);
+    assert.strictEqual(result2._type, 'translatableString');
     assert.ok(result2._string);
-    assert.equal(4, result2._string.length);
+    assert.strictEqual(result2._string.length, 4);
   });
 
   // prettier-ignore
@@ -92,41 +92,41 @@ describe('StringBuilder Join', function() {
     const result1 = StringBuilder.joinSentences([null, 'a', T('b'), 'c']);
     const result2 = StringBuilder.joinSentences([null, 'd', T('e'), 'f']);
     const result3 = StringBuilder.joinSentences([result1, result2, 'g', 'h', T('i'),]);
-    assert.equal('translatableString', result3._type);
+    assert.strictEqual(result3._type, 'translatableString');
     assert.ok(result3._string);
-    assert.equal(6, result3._string.length);
+    assert.strictEqual(result3._string.length, 6);
   });
 
   // prettier-ignore
   it('#Test joinWords', function() {
-    assert.equal(StringBuilder._toFlatten(StringBuilder.joinWords(['a',   'b']      )), "a b");
-    assert.equal(StringBuilder._toFlatten(StringBuilder.joinWords( 'a',   'b'       )), "a b");
-    assert.equal(StringBuilder._toFlatten(StringBuilder.joinWords(['a',   'b',  'c'])), "a b c");
-    assert.equal(StringBuilder._toFlatten(StringBuilder.joinWords( 'a',   'b',  'c' )), "a b c");
-    assert.equal(StringBuilder._toFlatten(StringBuilder.joinWords(['a', T('b'), 'c'])), "a @{b} c");
-    assert.equal(StringBuilder._toFlatten(StringBuilder.joinWords( 'a', T('b'), 'c' )), "a @{b} c");
+    assert.strictEqual(StringBuilder._toFlatten(StringBuilder.joinWords(['a',   'b']      )), "a b");
+    assert.strictEqual(StringBuilder._toFlatten(StringBuilder.joinWords( 'a',   'b'       )), "a b");
+    assert.strictEqual(StringBuilder._toFlatten(StringBuilder.joinWords(['a',   'b',  'c'])), "a b c");
+    assert.strictEqual(StringBuilder._toFlatten(StringBuilder.joinWords( 'a',   'b',  'c' )), "a b c");
+    assert.strictEqual(StringBuilder._toFlatten(StringBuilder.joinWords(['a', T('b'), 'c'])), "a @{b} c");
+    assert.strictEqual(StringBuilder._toFlatten(StringBuilder.joinWords( 'a', T('b'), 'c' )), "a @{b} c");
   });
 });
 
 describe('StringBuilder _toFlatten', function() {
   it('#Test empty', function() {
-    assert.equal(StringBuilder._toFlatten(''), '');
+    assert.strictEqual(StringBuilder._toFlatten(''), '');
   });
 
   // prettier-ignore
   it('#Test single', function() {
-    assert.equal(StringBuilder._toFlatten(  "coucou" ),                            "coucou");
-    assert.equal(StringBuilder._toFlatten(T("coucou")),                            "@{coucou}");
-    assert.equal(StringBuilder._toFlatten(StringBuilder.join([  "coucou" ], ".")), "coucou");
-    assert.equal(StringBuilder._toFlatten(StringBuilder.join([T("coucou")], ".")), "@{coucou}");
-    assert.equal(StringBuilder._toFlatten(T("a|b|coucou")),                        "@{a|b|coucou}");
+    assert.strictEqual(StringBuilder._toFlatten(  "coucou" ),                            "coucou");
+    assert.strictEqual(StringBuilder._toFlatten(T("coucou")),                            "@{coucou}");
+    assert.strictEqual(StringBuilder._toFlatten(StringBuilder.join([  "coucou" ], ".")), "coucou");
+    assert.strictEqual(StringBuilder._toFlatten(StringBuilder.join([T("coucou")], ".")), "@{coucou}");
+    assert.strictEqual(StringBuilder._toFlatten(T("a|b|coucou")),                        "@{a|b|coucou}");
   });
 
   // prettier-ignore
   it('#Test mix', function() {
-    assert.equal(StringBuilder._toFlatten(StringBuilder.join([  "a",    "b" ], ".")), "a.b");
-    assert.equal(StringBuilder._toFlatten(StringBuilder.join([T("a"), T("b")], ".")), "@{a}.@{b}");
-    assert.equal(StringBuilder._toFlatten(StringBuilder.join([T("a"),   "b" ], ".")), "@{a}.b");
-    assert.equal(StringBuilder._toFlatten(StringBuilder.join([  "a",  T("b")], ".")), "a.@{b}");
+    assert.strictEqual(StringBuilder._toFlatten(StringBuilder.join([  "a",    "b" ], ".")), "a.b");
+    assert.strictEqual(StringBuilder._toFlatten(StringBuilder.join([T("a"), T("b")], ".")), "@{a}.@{b}");
+    assert.strictEqual(StringBuilder._toFlatten(StringBuilder.join([T("a"),   "b" ], ".")), "@{a}.b");
+    assert.strictEqual(StringBuilder._toFlatten(StringBuilder.join([  "a",  T("b")], ".")), "a.@{b}");
   });
 });
