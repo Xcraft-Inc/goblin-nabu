954e3630fdab69299ab1d57c7ded35fc611cb68d
Mathieu Schroeter <mathieu@schroetersa.ch>
2024-04-21 22:55:14 +0200

    Add a new export for the T frontend function and a Tw React component

---
+++ widgets/helpers/nabu.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import _TT from '../t/widget.js';
+import _T from '../helpers/t.js';
+import T from './t-frontend.js';
+
+const Tw = (...args) => <_TT msgid={_T(...args)} />;
+
+export {T, Tw};
